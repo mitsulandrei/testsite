f902f516278b198bd1e0940d35ff1473418d0634
FIRST_NAME LAST_NAME <mitsulandrei@example.com>
2022-10-05 14:49:35 +0300

    validation

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -1,11 +1,28 @@
 from django import forms
-from .models import Category
+from .models import News
+import re
+from django.core.exceptions import ValidationError
 
-class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(attrs={"class":"form-control"}))
-    content = forms.CharField(label='Текст', required=False, widget=forms.Textarea(attrs={
-        "class":"form-control",
-        "rows": 5,
-    }))
-    is_published = forms.BooleanField(label='Опубликовано', initial=True)
-    category = forms.ModelChoiceField(empty_label='Выбрать', label='Категория:',queryset=Category.objects.all(), widget=forms.Select(attrs={"class": "form-select"}))
+# class NewsForm(forms.Form):
+#     title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(attrs={"class":"form-control"}))
+#     content = forms.CharField(label='Текст', required=False, widget=forms.Textarea(attrs={
+#         "class":"form-control",
+#         "rows": 5,
+#     }))
+#     is_published = forms.BooleanField(label='Опубликовано', initial=True)
+#     category = forms.ModelChoiceField(empty_label='Выбрать', label='Категория:',queryset=Category.objects.all(), widget=forms.Select(attrs={"class": "form-select"}))
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows':5}),
+            'category': forms.Select(attrs={'class': 'form-select'}),
+        }
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название не должно начинаться с цифры')
+        return title
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -10,7 +10,7 @@ class News(models.Model):
     update_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Статус новости')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория', null=True, blank=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={'news_id': self.pk})
